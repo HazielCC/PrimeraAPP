979b3a9f56b5bd96f6512450a1402c21157554d4
Haziel Covarrubias <67761409+HazielCC@users.noreply.github.com>
2024-12-04 18:57:40 -0700

    Eliminación de archivos innecesarios y adición de nuevos recursos gráficos y configuración de DataStore para ajustes de configuración.

---
+++ app/src/main/java/androidearly/core/Settings.kt
@@ -0,0 +1,13 @@
+package androidearly.provider.utilities
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+
+object Settings {
+
+}
