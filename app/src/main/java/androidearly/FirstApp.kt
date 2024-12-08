634ceb0d5c9304421286ca8855c80c680243357f
Haziel Covarrubias <67761409+HazielCC@users.noreply.github.com>
2024-12-07 19:05:48 -0700

    Añadidos nuevos archivos para la gestión de diálogos y notificaciones, así como la implementación de una verificación de conectividad de red.

---
+++ app/src/main/java/androidearly/FirstApp.kt
@@ -0,0 +1,4 @@
+package androidearly
+
+class FirstApp {
+}
---
+++ app/src/main/java/androidearly/utilities/services/API.kt
@@ -0,0 +1,14 @@
+package androidearly.provider.utilities
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+object API {
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
+    }
+}
